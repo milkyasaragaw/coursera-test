cbfbf553f50012439e0cb08275822bb7c0006cdb
Milkyas Aragaw <milkyasaragaw@gmail.com>
2021-05-18 16:51:10 +0300

    Updated

---
+++ module4_solution/SpeakHello.js
@@ -0,0 +1,9 @@
+(function (window){
+  var helloSpeaker = {};
+  helloSpeaker.name = name;
+  var speakWord = "Hello";
+  helloSpeaker.speak = function() {
+    console.log(speakWord + " " + helloSpeaker.name);
+  }
+window.helloSpeaker = helloSpeaker;
+})(window);
